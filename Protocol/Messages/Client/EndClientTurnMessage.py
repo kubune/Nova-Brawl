ea743f29bfc01294497e043cea20c1c3f05c0ed1
8Hacc <93487096+8-bitHacc@users.noreply.github.com>
2024-11-16 17:44:39 +0200

    New Commands implementation

--- Protocol/Messages/Client/EndClientTurnMessage.py
+++ Protocol/Messages/Client/EndClientTurnMessage.py
@@ -1,30 +1,46 @@
 from ByteStream.Reader import Reader
-from Utils.Helpers import Helpers
-from Protocol.LogicCommandManager import commands
+from Protocol.LogicCommandManager import LogicCommandManager
 
 class EndClientTurnMessage(Reader):
     def __init__(self, client, player, initial_bytes):
         super().__init__(initial_bytes)
         self.client = client
         self.player = player
+        self.tick: int = 0
+        self.checksum: int = 0
+        self.commands: list = []
 
     def decode(self):
         self.readVInt()
-        self.readVInt()
-        self.readVInt()
         self.tick = self.readVInt()
-        if self.tick != 0:
-            self.commandID = self.readVInt()
+        self.checksum = self.readVInt()
 
+        for x in range(self.readVInt()): # Commands Count = self.readVInt() in this case
+            commandID = self.readVInt()
+            self.commands.append({"id": commandID})
+
+            if LogicCommandManager.commandExists(commandID):
+                if LogicCommandManager.isServerToClient(commandID): continue
+                command = LogicCommandManager.createCommandByType(commandID)
+                if command:
+                    self.commands[x]["cls"] = command
+                    command.decode(self)
+                    print(f"CommandID: {commandID}, {command.__name__} handled!")
+                else:
+                    # Attempt to decode LogicCommand, a more proper reimplementation of Commands will come soon.
+                    self.readVInt()
+                    self.readVInt()
+                    self.readLogicLong()
+                    print(f"CommandID: {commandID}, {LogicCommandManager.getCommandName(commandID)} unhandled!")
+            else:
+                print(f"CommandID: {commandID} unhandled!")
 
     def process(self, db):
-        if self.tick != 0:
-            if self.commandID in commands:
-                command = commands[self.commandID]
-                try:
-                    command.decode(self)
-                    command.process(self, db)
-                except AttributeError:
-                    command(self.client, self.player).send() # Exception for OutOfSyncMessage
-            else:
-                print(f'{Helpers.cyan}[CLIENT] Unhandled Command! ID: {self.commandID}')
+        if not self.commands: return
+
+        for cmd in self.commands:
+            if "cls" not in cmd: return
+
+            if hasattr(cmd["cls"], "process"):
+                print(cmd["cls"].__class__.__name__)
+                cmd["cls"].process(self, db)
