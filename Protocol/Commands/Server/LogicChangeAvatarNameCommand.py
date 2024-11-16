11872dc407e5c9d8c153b84a8f30badbd138ea54
8Hacc <93487096+8-bitHacc@users.noreply.github.com>
2024-11-16 17:48:06 +0200

    New Commands implementation

--- Protocol/Commands/Server/LogicChangeAvatarNameCommand.py
+++ Protocol/Commands/Server/LogicChangeAvatarNameCommand.py
@@ -5,3 +5,6 @@ class LogicChangeAvatarNameCommand(Writer):
     def encode(self):
         self.writeString(self.player.name)
         self.writeVInt(1)
+
+    def getCommandType(self) -> int:
+        return 201
