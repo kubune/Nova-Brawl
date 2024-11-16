df3b4e3908db2e3e31079dd04d1000d787705d8b
8Hacc <93487096+8-bitHacc@users.noreply.github.com>
2024-11-16 17:48:50 +0200

    New Commands implementation

--- Protocol/Commands/Server/LogicSetSupportedCreatorCommand.py
+++ Protocol/Commands/Server/LogicSetSupportedCreatorCommand.py
@@ -6,3 +6,6 @@ class LogicSetSupportedCreatorCommand(Writer):
         self.writeVInt(1)
         self.writeString(self.player.content_creator)
         self.writeVInt(1)
+
+    def getCommandType(self) -> int:
+        return 215
